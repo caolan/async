c09010b1a441be5681874f3181e501dc92122963
Chris Breneman <crispy@cluenet.org>
2014-04-22 16:13:14 -0400

    Add tests and documentation for stackSafe

--- test/test-async.js
+++ test/test-async.js
@@ -618,7 +618,7 @@ exports['retry as an embedded task'] = function(test) {
     var retryResult = 'RETRY';
     var fooResults;
     var retryResults;
-    
+
     async.auto({
         foo: function(callback, results){
             fooResults = results;
@@ -2563,20 +2563,20 @@ exports['cargo bulk task'] = function (test) {
 };
 
 exports['cargo drain once'] = function (test) {
-   
+
    var c = async.cargo(function (tasks, callback) {
       callback();
     }, 3);
-    
+
     var drainCounter = 0;
     c.drain = function () {
       drainCounter++;
     }
-    
+
     for(var i = 0; i < 10; i++){
       c.push(i);
     }
-    
+
     setTimeout(function(){
       test.equal(drainCounter, 1);
       test.done();
@@ -2584,17 +2584,17 @@ exports['cargo drain once'] = function (test) {
 };
 
 exports['cargo drain twice'] = function (test) {
-    
+
     var c = async.cargo(function (tasks, callback) {
       callback();
     }, 3);
-    
+
     var loadCargo = function(){
       for(var i = 0; i < 10; i++){
         c.push(i);
       }
     };
-    
+
     var drainCounter = 0;
     c.drain = function () {
       drainCounter++;
@@ -2918,11 +2918,44 @@ exports['queue started'] = function(test) {
 
   var calls = [];
   var q = async.queue(function(task, cb) {});
-  
+
   test.equal(q.started, false);
   q.push([]);
   test.equal(q.started, true);
   test.done();
 
+};
+
+exports['stack safe initially synchronous'] = function(test) {
+    var flag = false;
+    async.stackSafe(function() {
+        test.equal(flag, false);
+        test.done();
+    });
+    flag = true;
+};
+
+exports['stack safe eventually asynchronous'] = function(test) {
+    for(var i = 0; i < 1000; i++) {
+        async.stackSafe(function() { });
+    }
+
+    var flag = false;
+    async.stackSafe(function() {
+        test.equal(flag, true);
+        test.done();
+    });
+    flag = true;
+};
+
+exports['stack safe eachSeries eventually asynchronous'] = function(test) {
+    var flag = false;
+    async.safeEachSeries(Array(1000), function(item, cb) {
+        cb();
+    }, function() {
+        test.equal(flag, true);
+        test.done();
+    });
+    flag = true;
 };
 
