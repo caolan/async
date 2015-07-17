2ec2068e420481c412361902ce654fddcd253cfc
charlierudolph <charles.rudolph@originate.com>
2015-07-17 16:01:09 -0700

    switch to firefox for travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
     files: ['mocha_test/**/*_test.js'],
     frameworks: ['browserify', 'mocha'],
     preprocessors: {
