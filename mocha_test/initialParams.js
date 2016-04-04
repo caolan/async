51157c1ad38d9d88e4be9f61a17d25bd710ca68f
bspates <bxs4361@g.rit.edu>
2016-04-03 17:23:00 -0700

    add unit test for context curring when using initialParams

---
+++ mocha_test/initialParams.js
@@ -0,0 +1,26 @@
+var initialParams = require('../lib/internal/initialParams');
+var expect = require('chai').expect;
+
+describe('initialParams', function() {
+  it('should curry any bound context to the wrapped function', function() {
+    var passContext = function(args, cb) {
+      cb(this);
+    };
+
+    // call bind after wrapping with initialParams
+    var contextOne = {context: "one"};
+    var postBind = initialParams.default(passContext);
+    postBind = postBind.bind(contextOne);
+    postBind([], function(ref) {
+      expect(ref).to.equal(contextOne);
+    });
+
+    // call bind before wrapping with initialParams
+    var contextTwo = {context: "two"};
+    var preBind = passContext.bind(contextTwo);
+    preBind = initialParams.default(preBind);
+    preBind([], function(ref) {
+      expect(ref).to.equal(contextTwo);
+    });
+  });
+});
