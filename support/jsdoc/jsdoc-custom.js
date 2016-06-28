dce18f0924ebc29f7a9a2eaaab692fe7bc8b7bab
Hubert Argasinski <argasinski.hubert@gmail.com>
2016-06-28 00:43:32 -0400

    fixed searhing issues

--- support/jsdoc/jsdoc-custom.js
+++ support/jsdoc/jsdoc-custom.js
@@ -30,7 +30,18 @@ $(function initSearchBar() {
             header: '<h3 class="search-bar-header">Source Files</h3>'
         }
     }).on('typeahead:select', function(ev, suggestion) {
-        var $el = document.getElementById('.' + suggestion)
-        $('html, body').animate({ scrollTop: $el.offsetTop }, 500);
+        var protocol = window.location.protocol
+        var host = window.location.host;
+        var currentPath = window.location.pathname;
+        // handle searching source files
+        if (suggestion.indexOf('.html') !== -1) {
+            window.location.href = protocol + '//' + host + '/' + suggestion;
+        // handle searching from one of the source files or the home page
+        } else if (currentPath !== '/docs.html') {
+            window.location.href = protocol + '//' + host + '/docs.html#.' + suggestion;
+        } else {
+            var $el = document.getElementById('.' + suggestion);
+            $('#main').animate({ scrollTop: $el.offsetTop }, 500);
+        }
     });
 });
