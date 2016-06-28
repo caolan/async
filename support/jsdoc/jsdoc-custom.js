93c24e117e1112b10a1995083fca8190723fd746
Hubert Argasinski <argasinski.hubert@gmail.com>
2016-06-28 01:52:51 -0400

    jsdoc: added searching through issues, @megawac

--- support/jsdoc/jsdoc-custom.js
+++ support/jsdoc/jsdoc-custom.js
@@ -11,6 +11,31 @@ $(function initSearchBar() {
         prefetch: './sourceFiles.json'
     });
 
+    var githubIssues = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: 'https://api.github.com/search/issues?q=%QUERY+repo:caolan/async',
+            cache: false,
+            wildcard: '%QUERY',
+            transform: function(response) {
+                return $.map(response.items, function(issue) {
+                    // if (issue.state !== 'open') {
+                    //     return null;
+                    // }
+                    return {
+                        url: issue.html_url,
+                        name: issue.number + ': ' + issue.title,
+                        number: issue.number
+                    };
+                }).sort(function(a, b) {
+                    return b.number - a.number;
+                });
+            }
+        }
+    });
+
+
     $('.typeahead').typeahead({
         hint: true,
         highlight: true,
@@ -29,12 +54,23 @@ $(function initSearchBar() {
         templates: {
             header: '<h3 class="search-bar-header">Source Files</h3>'
         }
+    },
+    {
+        name: 'Issues',
+        source: githubIssues,
+        display: 'name',
+        templates: {
+            header: '<h3 class="search-bar-header">Issues</h3>'
+        }
     }).on('typeahead:select', function(ev, suggestion) {
         var protocol = window.location.protocol
         var host = window.location.host;
         var currentPath = window.location.pathname;
-        // handle searching source files
-        if (suggestion.indexOf('.html') !== -1) {
+        // handle issues
+        if (typeof suggestion !== 'string') {
+            window.location.href = suggestion.url;
+        // handle source files
+        } else if (suggestion.indexOf('.html') !== -1) {
             window.location.href = protocol + '//' + host + '/' + suggestion;
         // handle searching from one of the source files or the home page
         } else if (currentPath !== '/docs.html') {
