ffd66850df894a5a961c8f2d21d75d0796448ce9
Alexander Early <alexander.early@gmail.com>
2017-03-24 14:17:23 -0700

    wrapped all collections methods

--- mocha_test/es2017/asyncFunctions.js
+++ mocha_test/es2017/asyncFunctions.js
@@ -10,11 +10,21 @@ module.exports = function () {
     }
 
     const input = [1, 2, 3];
+    const inputObj = {a: 1, b: 2, c: 3};
 
     it('should asyncify async functions', (done) => {
         async.asyncify(asyncIdentity)(42, (err, val) => {
             assert(val === 42);
             done(err);
+        })
+    });
+
+    it('should handle errors in async functions', (done) => {
+        async.asyncify(async function () {
+            throw new Error('thrown error')
+        })((err) => {
+            assert(err.message = 'thrown error');
+            done();
         })
     });
 
@@ -63,6 +73,28 @@ module.exports = function () {
         });
     });
 
+
+    it('should handle async functions in mapValues', (done) => {
+        async.mapValues(inputObj, asyncIdentity, (err, result) => {
+            expect(result).to.eql(inputObj);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in mapValuesLimit', (done) => {
+        async.mapValuesLimit(inputObj, 2, asyncIdentity, (err, result) => {
+            expect(result).to.eql(inputObj);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in mapValuesSeries', (done) => {
+        async.mapValuesSeries(inputObj, asyncIdentity, (err, result) => {
+            expect(result).to.eql(inputObj);
+            done(err);
+        });
+    });
+
     it('should handle async functions in filter', (done) => {
         async.filter(input, asyncIdentity, (err, result) => {
             expect(result).to.eql(input);
@@ -80,6 +112,143 @@ module.exports = function () {
     it('should handle async functions in filterSeries', (done) => {
         async.filterSeries(input, asyncIdentity, (err, result) => {
             expect(result).to.eql(input);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in reject', (done) => {
+        async.reject(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql([]);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in rejectLimit', (done) => {
+        async.rejectLimit(input, 2, asyncIdentity, (err, result) => {
+            expect(result).to.eql([]);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in rejectSeries', (done) => {
+        async.rejectSeries(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql([]);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in every', (done) => {
+        async.every(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in everyLimit', (done) => {
+        async.everyLimit(input, 2, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in everySeries', (done) => {
+        async.everySeries(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in some', (done) => {
+        async.some(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in someLimit', (done) => {
+        async.someLimit(input, 2, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in someSeries', (done) => {
+        async.someSeries(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(true);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in groupBy', (done) => {
+        async.groupBy(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql({1: [1], 2: [2], 3: [3]});
+            done(err);
+        });
+    });
+
+    it('should handle async functions in groupByLimit', (done) => {
+        async.groupByLimit(input, 2, asyncIdentity, (err, result) => {
+            expect(result).to.eql({1: [1], 2: [2], 3: [3]});
+            done(err);
+        });
+    });
+
+    it('should handle async functions in groupBySeries', (done) => {
+        async.groupBySeries(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql({1: [1], 2: [2], 3: [3]});
+            done(err);
+        });
+    });
+
+
+    it('should handle async functions in concat', (done) => {
+        async.concat(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(input);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in concatSeries', (done) => {
+        async.concatSeries(input, asyncIdentity, (err, result) => {
+            expect(result).to.eql(input);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in reduce', (done) => {
+        async.reduce(input, 0, async function (acc, val) {
+            var res = await Promise.resolve(acc + val);
+            return res;
+        },
+        (err, result) => {
+            expect(result).to.eql(6);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in reduceRight', (done) => {
+        async.reduceRight(input, 0, async function (acc, val) {
+            var res = await Promise.resolve(acc + val);
+            return res;
+        },
+        (err, result) => {
+            expect(result).to.eql(6);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in sortBy', (done) => {
+        async.sortBy([3, 2, 1], asyncIdentity, (err, result) => {
+            expect(result).to.eql(input);
+            done(err);
+        });
+    });
+
+    it('should handle async functions in transform', (done) => {
+        async.transform(inputObj, async function (obj, val, key) {
+            obj[key] = await Promise.resolve(val);
+        }, (err, result) => {
+            expect(result).to.eql(inputObj);
             done(err);
         });
     });
