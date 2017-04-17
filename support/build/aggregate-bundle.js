8fba4e5398f7c3cec8ab5f53986f80e8fb5c2dbf
Alexander Early <alexander.early@gmail.com>
2017-04-16 21:28:45 -0700

    move support functions into scripts that call them

--- support/build/aggregate-bundle.js
+++ support/build/aggregate-bundle.js
@@ -1,7 +1,15 @@
-const aggregateBuild = require('./aggregate-build');
+const {rollup} = require('rollup');
+const nodeResolve = require('rollup-plugin-node-resolve');
 
-aggregateBuild({
-    entriesPath: 'build-es',
-    outfile: 'build/dist/async.js',
-    format: 'umd',
-}, err => { if (err) throw err; })
+rollup({
+    entry: 'build-es/index.js',
+    plugins: [ nodeResolve() ]
+})
+.then(function ( bundle ) {
+    return bundle.write({
+        format: 'umd',
+        moduleName: 'async',
+        dest: 'build/dist/async.js'
+    });
+})
+.catch((err) => { throw err; });
