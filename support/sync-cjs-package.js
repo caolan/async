f4dc8e4e3cc8b408549581b2111b919774414cf5
Alexander Early <alexander.early@gmail.com>
2019-06-22 20:00:51 -0700

    fix: include config for browserify.  Closes #1653

--- support/sync-cjs-package.js
+++ support/sync-cjs-package.js
@@ -3,5 +3,9 @@
 var fs = require('fs');
 var json = JSON.parse(fs.readFileSync(__dirname + "/../package.json"), "utf8");
 json.module = 'dist/async.mjs'
+// mark this as an ES6 module for browserify
+json.browserify = {
+    transform: [["babelify", { presets: ["@babel/preset-env"] }]]
+}
 
 process.stdout.write(JSON.stringify(json, null, 2));
